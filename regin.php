a56a21f8e1033b95f8851e0c2f0285e2c5df99e0
Ivan Leushkanov <ivanleushkanov@mail.ru>
2025-03-11 22:44:38 +0500

    Проверка пароля на маску. Леушканов

--- regin.php
+++ regin.php
@@ -37,16 +37,24 @@
 				<div class = "login">
 					<div class="name">Регистрация</div>
 				
-					<div class = "sub-name">Логин:</div>
-					<input name="_login" type="text" placeholder="" onkeypress="return PressToEnter(event)"/>
-					<div class = "sub-name">Пароль:</div>
-					<input name="_password" type="password" placeholder="" onkeypress="return PressToEnter(event)"/>
-					<div class = "sub-name">Повторите пароль:</div>
-					<input name="_passwordCopy" type="password" placeholder="" onkeypress="return PressToEnter(event)"/>
-					
-					<a href="login.php">Вернуться</a>
-					<input type="button" class="button" value="Зайти" onclick="RegIn()" style="margin-top: 0px;"/>
-					<img src = "img/loading.gif" class="loading" style="margin-top: 0px;"/>
+					<form id="registrationForm" onsubmit="return false;">
+						<div class="sub-name">Логин:</div>
+						<input name="_login" type="text" placeholder="Введите логин" required/>
+
+						<div class="sub-name">Email:</div>
+						<input name="_email" type="email" placeholder="Введите email" required/>
+
+						<div class="sub-name">Пароль:</div>
+						<input name="_password" type="password" placeholder="Введите пароль" required/>
+
+						<div class="sub-name">Повторите пароль:</div>
+						<input name="_passwordCopy" type="password" placeholder="Повторите пароль" required/>
+
+						<input type="button" class="button" value="Зарегистрироваться" onclick="RegIn()" style="margin-top: 10px; margin-bottom: 10px; width: 100%;"/>
+						<a href="login.php">Вернуться</a>
+						
+						<img src="img/loading.gif" class="loading" style="margin-top: 0px; display: none;"/>
+					</form>
 				</div>
 				
 				<div class="footer">
@@ -61,55 +69,94 @@
 			var loading = document.getElementsByClassName("loading")[0];
 			var button = document.getElementsByClassName("button")[0];
 			
+			function validatePassword(password) {
+				if (password.length <= 8) {
+					return "Пароль должен содержать более 8 символов.";
+				}
+				if (!/[a-zA-Z]/.test(password)) {
+					return "Пароль должен содержать латинские буквы.";
+				}
+				if (!/\d/.test(password)) {
+					return "Пароль должен содержать хотя бы одну цифру.";
+				}
+				if (!/[!@#$%^&*(),.?"-_:{}|<>]/.test(password)) {
+					return "Пароль должен содержать хотя бы один специальный символ.";
+				}
+				if (!/[A-Z]/.test(password)) {
+					return "Пароль должен содержать хотя бы одну заглавную букву.";
+				}
+				return true;
+			}
+
 			function RegIn() {
-				var _login = document.getElementsByName("_login")[0].value;
-				var _password = document.getElementsByName("_password")[0].value;
-				var _passwordCopy = document.getElementsByName("_passwordCopy")[0].value;
-				
-				if(_login != "") {
-					if(_password != "") {
-						if(_password == _passwordCopy) {
-							loading.style.display = "block";
-							button.className = "button_diactive";
-							
-							var data = new FormData();
-							data.append("login", _login);
-							data.append("password", _password);
-							
-							// AJAX запрос
-							$.ajax({
-								url         : 'ajax/regin_user.php',
-								type        : 'POST', // важно!
-								data        : data,
-								cache       : false,
-								dataType    : 'html',
-								// отключаем обработку передаваемых данных, пусть передаются как есть
-								processData : false,
-								// отключаем установку заголовка типа запроса. Так jQuery скажет серверу что это строковой запрос
-								contentType : false, 
-								// функция успешного ответа сервера
-								success: function (_data) {
-									console.log("Авторизация прошла успешно, id: " +_data);
-									if(_data == -1) {
-										alert("Пользователь с таким логином существует.");
-										loading.style.display = "none";
-										button.className = "button";
-									} else {
-										location.reload();
-										loading.style.display = "none";
-										button.className = "button";
-									}
-								},
-								// функция ошибки
-								error: function( ){
-									console.log('Системная ошибка!');
-									loading.style.display = "none";
-									button.className = "button";
-								}
-							});
-						} else alert("Пароли не совподают.");
-					} else alert("Введите пароль.");
-				} else alert("Введите логин.");
+				var _login = document.getElementsByName("_login")[0].value.trim();
+				var _email = document.getElementsByName("_email")[0].value.trim();
+				var _password = document.getElementsByName("_password")[0].value.trim();
+				var _passwordCopy = document.getElementsByName("_passwordCopy")[0].value.trim();
+
+				if (_login === "") {
+					alert("Введите логин.");
+					return;
+				}
+				if (_email === "") {
+					alert("Введите email.");
+					return;
+				}
+				if (_password === "") {
+					alert("Введите пароль.");
+					return;
+				}
+				if (_password !== _passwordCopy) {
+					alert("Пароли не совпадают.");
+					return;
+				}
+
+				var passwordValidationResult = validatePassword(_password);
+				if (passwordValidationResult !== true) {
+					alert(passwordValidationResult);
+					return;
+				}
+
+				loading.style.display = "block";
+				button.className = "button_diactive";
+
+				var data = new FormData();
+				data.append("login", _login);
+				data.append("email", _email);
+				data.append("password", _password);
+
+				$.ajax({
+					url: 'ajax/regin_user.php',
+					type: 'POST',
+					data: data,
+					cache: false,
+					dataType: 'html',
+					processData: false,
+					contentType: false,
+					success: function (response) {
+						console.log("Ответ сервера:", response); // Отладочное сообщение
+						try {
+							var data = JSON.parse(response);
+							if (data.status === "error") {
+								alert(data.message);
+							} else {
+								alert(data.message);
+								//window.location.href = "verify_code.php";
+							}
+						} catch (e) {
+							console.error("Ошибка при обработке ответа сервера:", e);
+							alert("Произошла ошибка. Пожалуйста, попробуйте позже.");
+						}
+					},
+					error: function () {
+						console.log('Системная ошибка!');
+						alert("Не удалось выполнить запрос к серверу.");
+					},
+					complete: function () {
+						loading.style.display = "none";
+						button.className = "button";
+					}
+				});
 			}
 			
 			function PressToEnter(e) {
